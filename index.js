128e9b9d773f34d70f03b6289bd155edd5919f57
Amen Atikpo <alaskale2024@gmail.com>
2022-06-26 08:38:13 +0000

    Context Lab

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
 /* Your Code Here */
+function createEmployeeRecord(record){
+    return {
+        firstName: record[0],
+        familyName: record[1],
+        title: record[2],
+        payPerHour: record[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }  
+}
+function createEmployeeRecords(employeeData){
+    return employeeData.map(function(rows){
+        return createEmployeeRecord(rows)
+    })
+}
+function createTimeInEvent(dateStamp){
+    
+    
+}
+
+function createTimeOutEvent(){
+
+}
+
+function hoursWorkedOnDate(){
+
+}
+
+function hoursWorkedOnDate(){
+
+}
+
+function wagesEarnedOnDate(){
+
+}
+
+
+
+function calculatePayroll() {
+
+}
+
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
